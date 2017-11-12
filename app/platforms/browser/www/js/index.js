2a886d83a4e40fce8b73793d893a034ca4c2c647
meguia25 <marieguiaestrade@gmail.com>
2017-11-12 20:23:06 -0300

    segundas pantallas

--- app/platforms/browser/www/js/index.js
+++ app/platforms/browser/www/js/index.js
@@ -16,39 +16,12 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-        $("#btnEntrenamiento").click(irEntrenamiento);
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+ $(document).ready(inicio);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function inicio(){
 
-        console.log('Received Event: ' + id);
-    }
-    function irEntrenamiento(){
-      
-    }
+}
+
+
 
 };
